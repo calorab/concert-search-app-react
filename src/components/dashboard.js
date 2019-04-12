60d54f80d936f23281b36ac4c0d554fb4b547d6f
Caleb Gammon <calebwgammon@gmail.com>
2019-04-12 11:49:09 -0400

    added this.loadArtist to test local API connection

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,11 +1,50 @@
 import React from 'react';
-
+import {API_BASE_URL} from './config.js';
 //import {addArtist} from '../actions';
 
 import SearchArtists from './searchartist';
 import FollowedArtists from './followedartists';
 
 export default class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: null
+        };
+    }
+
+
+    componentDidMount() {
+        this.loadArtist();
+    }
+
+
+    loadArtist() {
+        this.setState({
+            error: null
+        });
+        return fetch(`${API_BASE_URL}/api/*`)
+            .then(res => {
+                if (!res.ok) {
+                    return Promise.reject(res.statusText);
+                }
+                return res.json();
+                })
+            .then(() =>
+                    this.setState({
+                    error: null
+            })
+                 )
+            .catch(err =>
+                   this.setState({
+                        error: 'Could not load board'
+                    })
+            );
+    }
+
+
+
     render() {
         return (
             <div>
