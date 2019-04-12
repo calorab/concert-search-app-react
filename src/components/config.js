dbaf73dcee7a16cededcb7993c1b1cd91be1332a
Caleb Gammon <calebwgammon@gmail.com>
2019-04-12 06:26:09 -0400

    updated url link

--- src/components/config.js
+++ src/components/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'mongodb+srv://admin:12345@cluster0-6akq9.mongodb.net/test?retryWrites=true';
+      process.env.REACT_APP_API_BASE_URL || 'https://floating-savannah-68896.herokuapp.com/';
