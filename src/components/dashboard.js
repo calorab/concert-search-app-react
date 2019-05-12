3d0a06419f6b975c1bace4257ef71fa581f6f01c
Caleb Gammon <calebwgammon@gmail.com>
2019-05-12 15:56:30 -0400

    added require-login and dashboard without details

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,15 +2,50 @@ import React from 'react';
 
 import SearchArtists from './searchartist';
 import FollowedArtists from './followedartists';
-
-export default class Dashboard extends React.Component {
-
+// import {connect} from 'react-redux';
+import RequireLogin from './require-login';
+import {fetchProtectedData} from '../actions/protected-data';
+//CALEB ------- update to dispatch GET followed artists or whatever
+export class Dashboard extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(fetchProtectedData());
+    }
+//CALEB ------- Correct this to add followered rtists and search artists to the return
     render() {
         return (
-            <div>
-                <SearchArtists />
-                <FollowedArtists />
+            <div className="dashboard">
+                <div className="dashboard-username">
+                    Username: {this.props.username}
+                </div>
+                <div className="dashboard-name">Name: {this.props.name}</div>
+                <div className="dashboard-protected-data">
+                    Protected data: {this.props.protectedData}
+                </div>
             </div>
         );
     }
 }
+
+const mapStateToProps = state => {
+    const {currentUser} = state.auth;
+    return {
+        username: state.auth.currentUser.username,
+        name: `${currentUser.firstName} ${currentUser.lastName}`,
+        protectedData: state.protectedData.data
+    };
+};
+
+export default requiresLogin()(connect(mapStateToProps)(Dashboard));
+
+
+// export default class Dashboard extends React.Component {
+
+//     render() {
+//         return (
+//             <div>
+//                 <SearchArtists />
+//                 <FollowedArtists />
+//             </div>
+//         );
+//     }
+// }
