f93e789859260c0db0a6da0f90edaa91c800d424
Caleb Gammon <calebwgammon@gmail.com>
2019-05-21 19:57:22 -0400

    small changes while working on CORS

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -7,7 +7,6 @@ import {required, nonEmpty, matches, length, isTrimmed} from '../validators';
 const passwordLength = length({min: 8, max: 72});
 const matchesPassword = matches('password');
 
-//removed firstName and lastName from lines 13-14 and commented out line 32-35
 export class RegistrationForm extends React.Component {
     onSubmit(values) {
 //        event.preventDefault();
